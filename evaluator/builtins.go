bcad2b6645b76231e352482820848df99a16e5cc
fiorek <marcin.lukasz.fiorek@gmail.com>
2025-05-03 17:04:20 +0200

    Chapter 4.6: The Grand Finale

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "mfiorek/waiig/object"
+import (
+	"fmt"
+	"mfiorek/waiig/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -95,6 +98,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
